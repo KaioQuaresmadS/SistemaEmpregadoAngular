a6e7fc5765c54cf66af5efa88831fa989f216a31
Kaio Quaresma <kaioquaresmadesouza1@gmail.com>
2025-04-21 21:41:47 -0300

    first commit

---
+++ SistemaEmpregados/Migrations/20250419191345_CreateTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SistemaEmpregados.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cadastrosEmpregados",
+                columns: table => new
+                {
+                    EmpregadorId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    firstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    lastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    contantNo = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    City = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Address = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cadastrosEmpregados", x => x.EmpregadorId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cadastrosEmpregados");
+        }
+    }
+}
